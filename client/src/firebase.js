0848ea468bf1da17b02d6a0177543d9f36119a1f
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2024-02-22 01:05:45 -0500

    fix: managed buttons css

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -15,7 +15,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const analytics = getAnalytics(app)
 const storage = getStorage(app);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-export { app, db, storage, googleProvider, auth};
+export { app, db, storage, analytics, googleProvider, auth};
