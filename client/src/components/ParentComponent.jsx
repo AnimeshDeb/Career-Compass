e177cf1d22f70f9283aa175d5cdada67af38f493
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2024-03-02 16:25:30 -0500

    fix & refactor: sign up to user connection and edit mode

--- client/src/components/ParentComponent.jsx
+++ client/src/components/ParentComponent.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import { useState } from "react";
 import SeekerIntro from "./SeekerSignProcessIntro";
 import SeekerSkills from "./SeekerSignupProcessSkills";
 import SeekerChallenges from "./SeekerChallengers";
@@ -6,62 +6,68 @@ import { useLocation } from "react-router-dom";
 import SeekerEducation from "./SeekerEducation";
 import SeekerJobs from "./SeekerJobs";
 import SeekerProfilepic from "./SeekerProfilepic";
-import UserpageSeeker from "../routes/seeker/userpage/userpageSeeker";
-import {useNavigate} from "react-router-dom";
-function ParentComponent(){
-    const navigate=useNavigate();
-    const location=useLocation();
-    const name=location.state?.fullName;
-    console.log("name value is"+ name);
-    const[step, setStep]=useState(0);
+import { useNavigate } from "react-router-dom";
+function ParentComponent() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const name = location.state?.fullName;
+  const [step, setStep] = useState(0);
 
-    const handleNextStep=()=>{
-        console.log("reaching parent function");
-        // switch(step){
-        //     case 0:
-        //         navigate("/SeekerIntro");
-        //         break;
-        //     case 1:
-        //         navigate("/SeekerSkills");
-        //         break;
-        //     case 2:
-        //         navigate("/SeekerChallenges");
-        //         break;
-        //     case 3:
-        //         navigate("/SeekerEducation");
-        //         break;
-        //     case 4:
-        //         navigate("/SeekerJobs");
-        //         break;
-        //     case 5:
-        //         navigate("/SeekerProfilepic");
-        //         break;
-        //     case 6:
-        //         navigate("/SeekerUserpage");
-        //         break;
-
-        // }
-        setStep(step+1);
-        
-        
-    };
-    const handlePrevStep=()=>{
-        setStep(step-1);
-        
+  const handleNextStep = () => {
+    if (step < 5) {
+      setStep(step + 1);
+    } else {
+      console.log(step);
+      navigate("/user", { state: { name: name } });
     }
-    return(
-        <>
-        {step===0 && <SeekerIntro handleNextStep={handleNextStep} name={name} />}
-        {step===1 && <SeekerSkills handleNextStep={handleNextStep} name={name} handlePrevStep={handlePrevStep}/>}
-        {step===2 && <SeekerChallenges handleNextStep={handleNextStep} name={name} handlePrevStep={handlePrevStep}/>}
-        {step===3 && <SeekerEducation handleNextStep={handleNextStep} name={name} handlePrevStep={handlePrevStep}/>}
-        {step===4 && <SeekerJobs handleNextStep={handleNextStep} name={name} handlePrevStep={handlePrevStep} />}
-        {step===5 && <SeekerProfilepic handleNextStep={handleNextStep} name={name} handlePrevStep={handlePrevStep}/>}
-        {step===6 && <UserpageSeeker  name={name} />}
-
-        </>
-    );
+  };
+  const handlePrevStep = () => {
+    if (step > 0) {
+      setStep(step - 1);
+    }
+  };
+  return (
+    <>
+      {step === 0 && (
+        <SeekerIntro handleNextStep={handleNextStep} name={name} />
+      )}
+      {step === 1 && (
+        <SeekerSkills
+          handleNextStep={handleNextStep}
+          name={name}
+          handlePrevStep={handlePrevStep}
+        />
+      )}
+      {step === 2 && (
+        <SeekerChallenges
+          handleNextStep={handleNextStep}
+          name={name}
+          handlePrevStep={handlePrevStep}
+        />
+      )}
+      {step === 3 && (
+        <SeekerEducation
+          handleNextStep={handleNextStep}
+          name={name}
+          handlePrevStep={handlePrevStep}
+        />
+      )}
+      {step === 4 && (
+        <SeekerJobs
+          handleNextStep={handleNextStep}
+          name={name}
+          handlePrevStep={handlePrevStep}
+        />
+      )}
+      {step === 5 && (
+        <SeekerProfilepic
+          handleNextStep={handleNextStep}
+          name={name}
+          handlePrevStep={handlePrevStep}
+        />
+      )}
+    </>
+  );
 }
 
-
-export default ParentComponent
+export default ParentComponent;
