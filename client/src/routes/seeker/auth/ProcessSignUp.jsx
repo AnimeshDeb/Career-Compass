401e30d3a124c3aa147ffaef2624cfeb52187424
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2024-03-06 22:19:11 -0500

    feat: Added animated progress bar

--- client/src/routes/seeker/auth/ProcessSignUp.jsx
+++ client/src/routes/seeker/auth/ProcessSignUp.jsx
@@ -10,24 +10,6 @@ import { useNavigate } from "react-router-dom";
 import "../../../styling/UploadImages.css";
 import NavbarWhite from "../../../components/navbar/version2/navbar";
 import anime from "animejs";
-const Step = ({ stepNumber, currentStep, label, goToStep, isCompleted }) => {
-  // Base styling for the step container
-  const baseStyle = "cursor-pointer flex justify-center items-center <h-2></h-2> pt-0 pb-0 p-2 ";
-  const completedStyle = "bg-secondary text-white ";
-  const activeStyle = "bg-primary text-white ";
-  const defaultStyle = "bg-gray-600 text-white ";
-  const textStyle = "text-center text-sm sm:text-base md:text-lg lg:text-xl ";
-
-  // Determine the step's current styling
-  let stepStyle = baseStyle + defaultStyle;
-  if (isCompleted) stepStyle += completedStyle;
-  if (stepNumber === currentStep) stepStyle += activeStyle;
-
-  return (
-    <div className={stepStyle + "flex-1"} onClick={() => goToStep(stepNumber)} title={label}>
-    </div>
-  );
-};
 
 function ParentComponent() {
   const navigate = useNavigate();
@@ -37,32 +19,44 @@ function ParentComponent() {
   const [step, setStep] = useState(0);
   const [showWelcome, setShowWelcome] = useState(true);
   const steps = [
-      "Introduction",
-      "Skills",
-      "Challenges",
-      "Education",
-      "Jobs",
-      "Profile Picture"
-    ];
+    "Introduction",
+    "Skills",
+    "Challenges",
+    "Education",
+    "Jobs",
+    "Profile Picture",
+  ];
 
-  const goToStep = (stepIndex) => {
-    if (stepIndex <= step) { // Prevent going forward but allow backward and direct jump to completed steps
-      setStep(stepIndex);
-    }
-  };
-  
+  useEffect(() => {
+    anime({
+      targets: ".progress-bar-fill",
+      width: [
+        (((step - 1) / steps.length) * 100).toFixed(0) + "%",
+        ((step / steps.length) * 100).toFixed(0) + "%",
+      ],
+      easing: "easeInOutQuad",
+      duration: 1000,
+    });
+  }, [step]);
+
   useEffect(() => {
     positionWelcomeContainer();
   }, []);
 
   const positionWelcomeContainer = () => {
-    const navbarHeight = document.querySelector('.navbar').offsetHeight;
-    document.querySelector('.welcome-container').style.top = `${navbarHeight}px`;
+    const navbarElement = document.querySelector(".navbar");
+    const welcomeContainerElement =
+      document.querySelector(".welcome-container");
+
+    if (navbarElement && welcomeContainerElement) {
+      const navbarHeight = navbarElement.offsetHeight;
+      welcomeContainerElement.style.top = `${navbarHeight}px`;
+    }
   };
   const handleNextClick = () => {
     anime({
       targets: ".welcome-container",
-      translateY: [0, -window.innerHeight ], 
+      translateY: [0, -window.innerHeight],
       easing: "easeInExpo",
       duration: 1500,
       complete: () => {
@@ -88,17 +82,8 @@ function ParentComponent() {
       <div className="navbar sticky top-0 z-50 p-0 bg-white">
         <NavbarWhite />
       </div>
-      <div className="w-full bg-gray-200 h-4 flex">
-        {steps.map((label, index) => (
-          <Step
-            key={index}
-            stepNumber={index}
-            currentStep={step}
-            label={label}
-            goToStep={goToStep}
-            isCompleted={index <= step}
-          />
-        ))}
+      <div className="w-full bg-gray-200 h-4 overflow-hidden">
+        <div className="progress-bar-fill bg-secondary h-4"></div>
       </div>
       {showWelcome && (
         <div className="welcome-container w-full h-full fixed inset-x-0 mx-auto flex items-center justify-center flex-col bg-primary text-white z-10 ">
